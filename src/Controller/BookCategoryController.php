237470e5e3eb96d0071420cd0dfa328a87093422
Evgeniy Pavlenko <pavlenkoevgeniy85@gmail.com>
2025-04-17 05:08:29 +0200

    linter

--- src/Controller/BookCategoryController.php
+++ src/Controller/BookCategoryController.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Service\BookCategoryService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class BookCategoryController extends AbstractController
 {
@@ -16,7 +16,7 @@ final class BookCategoryController extends AbstractController
     {
     }
 
-    #[Route('/api/v1/book/categories' , name:'api_v1_book_categories', methods: [Request::METHOD_GET])]
+    #[Route('/api/v1/book/categories', name: 'api_v1_book_categories', methods: [Request::METHOD_GET])]
     public function categories(): JsonResponse
     {
         return $this->json($this->bookCategoryService->getBookCategories());
